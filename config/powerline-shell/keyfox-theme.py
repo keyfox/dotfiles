c2c0d4f693dba97bdc021e5fcc7c7f2b197ff604
Keyfox <dev.keyfox@gmail.com>
2021-12-27 22:21:44 +0900

    Add colors per hostname

--- config/powerline-shell/keyfox-theme.py
+++ config/powerline-shell/keyfox-theme.py
@@ -1,6 +1,14 @@
 from powerline_shell.themes.default import DefaultColor
+from socket import gethostname
 
+HOSTNAME_FG_COLORS = {
+    "nebula": 160,
+    "yamabuki": 178,
+    "GGEAR2018": 45,
+    "pixen": 197,
+}
+HOSTNAME_FG_FALLBACK = DefaultColor.HOSTNAME_FG
 
 class Color(DefaultColor):
-    pass
+    HOSTNAME_FG = HOSTNAME_FG_COLORS.get(gethostname(), HOSTNAME_FG_FALLBACK)
 
