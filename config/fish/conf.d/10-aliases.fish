821b678565d6b32b0adb6b8c380e7eaa1ff27e6b
Keyfox <dev.keyfox@gmail.com>
2022-06-08 19:57:17 +0900

    Use k4x-genalias to register aliases in fish

--- config/fish/conf.d/10-aliases.fish
+++ config/fish/conf.d/10-aliases.fish
@@ -1,18 +1,3 @@
-#!/usr/bin/env fish
-for actual_command in python3.10 python3.9 python3.8 python3.7
-  if type -q $actual_command
-    alias python3=$actual_command
-    break
-  end
+for k in (~/.local/bin/k4x-genalias)
+  eval $k
 end
-
-if type -q 'python3'
-  alias python='python3'
-end
-
-if type -q 'pip3'
-  alias pip='pip3'
-end
-
-alias editcron='crontab -e'
-
