9ad884fec2cb289e2d8e8b0ec9ba0fec18c70c5d
Keyfox <12883873+keyfox@users.noreply.github.com>
2026-09-02 08:42:58 +0000

    Add ASCII-friendly prompt configuration for ASCII-only terminal

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,6 +1,11 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
+    # use ASCII-friendly prompt when using raw console
+    if test "$TERM" = linux
+        fish_config prompt choose default >/dev/null
+    end
+
     # add PATH
     fish_add_path ~/.local/bin
 end
