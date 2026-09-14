d226621a98395ff3070d6de08ddad0caecc4ade0
Keyfox <12883873+keyfox@users.noreply.github.com>
2026-09-14 07:48:12 +0000

    Add hostname color

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,3 +1,44 @@
+function hostname_color --description 'Generate a deterministic color from a hostname'
+    if test (count $argv) -ne 1
+        echo "usage: hostname_color HOSTNAME" >&2
+        return 2
+    end
+
+    set -l host $argv[1]
+
+    set -l colors \
+        8A435B \
+        8D434B \
+        8D453B \
+        8B492A \
+        864E18 \
+        7E5403 \
+        735A00 \
+        666005 \
+        57651A \
+        446A2C \
+        2C6D3E \
+        036F4F \
+        006F60 \
+        006E6F \
+        006C7D \
+        006888 \
+        176490 \
+        345F95 \
+        485996 \
+        585493 \
+        674F8D \
+        734B84 \
+        7D4778 \
+        84446A
+
+    set -l checksum (printf '%s' "$host" | cksum)
+    set -l hash (string split ' ' $checksum)[1]
+    set -l index (math "$hash % "(count $colors)" + 1")
+
+    echo $colors[$index]
+end
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
@@ -5,6 +46,8 @@ if status is-interactive
     if test "$TERM" = linux
         fish_config prompt choose default >/dev/null
     end
+
+    set -gx tide_pwd_bg_color (hostname_color (hostname -s))
 
     # add PATH
     fish_add_path ~/.local/bin
